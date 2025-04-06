81fa002e83b8c60a25c048feef0a458424b77aed
XpArKeR <xparker@gmx.net>
2025-04-06 16:00:39 +0200

    Inventory

---
+++ Code/ldjam57/Assets/Scripts/Core/Definitons/Inventories/MiningToolDefinitionInventoryItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Assets.Scripts.Core.Definitons.Inventories
+{
+    public class MiningToolDefinitionInventoryItem
+    {
+        public Int32? Amount { get; set; }
+        public MiningToolDefinition MiningTool { get; set; }
+    }
+}
