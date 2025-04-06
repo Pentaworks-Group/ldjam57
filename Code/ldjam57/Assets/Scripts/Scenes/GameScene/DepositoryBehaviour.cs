88a9ca5c18885f937a2d27dd0a38d24a920ca4d3
XpArKeR <xparker@gmx.net>
2025-04-07 00:47:16 +0200

    Extracted Depository to prefab.

--- Code/ldjam57/Assets/Scripts/Scenes/GameScene/DepositoryBehaviour.cs
+++ Code/ldjam57/Assets/Scripts/Scenes/GameScene/DepositoryBehaviour.cs
@@ -4,6 +4,8 @@ using Assets.Scripts.Core.Model;
 
 using GameFrame.Core.Extensions;
 
+using UnityEditor.UIElements;
+
 using UnityEngine;
 
 namespace Assets.Scripts.Scenes.GameScene
@@ -25,50 +27,54 @@ namespace Assets.Scripts.Scenes.GameScene
             levelRenderer.color = depository.Mineral.Color.ToUnity();
         }
 
-        private Boolean addDirection = true;
-
         public void OnClicked()
         {
-            if (addDirection)
-            {
-                AddMineral(5000);
-
-                var factor = depository.Value / depository.Capacity;
-
-                if (factor > 0.9d)
-                {
-                    addDirection = false;
-                }
-            }
-            else
-            {
-                SubtractMineral(5000);
-
-                var factor = depository.Value / depository.Capacity;
-
-                if (factor < 0.05)
-                {
-                    addDirection = true;
-                }
-            }
+            // Open Shop?
         }
 
-        public void AddMineral(Double amount)
+        /// <summary>
+        /// Adds given amount to deposit.
+        /// </summary>
+        /// <param name="amount">amount to be added</param>
+        /// <param name="overflow">Overflow if amount exceeds capacity</param>
+        /// <returns>True of successful, false if capacity is exceeded. Returns the <paramref name="overflow"/> if false.</returns>
+        public Boolean AddMineral(Double amount, out Double overflow)
         {
+            var result = true;
+
+            overflow = 0;
+
             this.depository.Value += amount;
-            UpdateLevel();
-        }
 
-        public void SubtractMineral(Double amount)
-        {
-            this.depository.Value -= amount;
-
-            if (this.depository.Value < 0)
+            if (this.depository.Value > this.depository.Capacity)
             {
-                this.depository.Value = 0;
+                overflow = this.depository.Value - this.depository.Capacity;
+                this.depository.Value = this.depository.Capacity;
+
+                result = false;
             }
 
             UpdateLevel();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Subtracts the given amount from deposit.
+        /// </summary>
+        /// <param name="amount">Value to be subtracted</param>
+        /// <returns>True if subtract was successful, otherwise false.</returns>
+        public Boolean SubtractMineral(Double amount)
+        {
+            if (amount > this.depository.Value)
+            {
+                this.depository.Value -= amount;
+                UpdateLevel();
+
+                return true;
+            }
+
+            return false;
         }
 
         private void UpdateLevel()
