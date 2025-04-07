7939ab48d22531d5bc82bf398c18a256a078489d
Lea Scherer <38538244+leas251@users.noreply.github.com>
2025-04-07 07:59:24 +0200

    Money Behaviour

--- Code/ldjam57/Assets/Scripts/Scenes/GameScene/MoneyBehaviour.cs
+++ Code/ldjam57/Assets/Scripts/Scenes/GameScene/MoneyBehaviour.cs
@@ -1,6 +1,11 @@
+using Assets.Scripts.Scenes.GameScene;
+using TMPro;
 using UnityEngine;
 
-	public class MoneyBehaviour : MonoBehaviour
-	{
-
-	}
+public class MoneyBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	TextMeshProUGUI cashLabel;
+	[SerializeField]
+	DiggerBehaviour diggerBehaviour;
+}
